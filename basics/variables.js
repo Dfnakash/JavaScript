95f37e6950f54296009edbc237b9b65e1c089d8f
AKASH TOMAR <121921767+Dfnakash@users.noreply.github.com>
2024-12-09 16:32:49 +0000

    JavaScript

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const studentId=123
+let studentName = "Akash Tomar"
+var studentEmail = "tomarakash@gmail.com"
+studenCity = "Lucknow"
+let studentState;
+
+console.log(studentId)
+console.table([ studentId,studentName,studentEmail,studenCity,studentState])
