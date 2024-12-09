77e77cd52eb5debb92449e6bcee68b41ffbdfda2
AKASH TOMAR <121921767+Dfnakash@users.noreply.github.com>
2024-12-09 16:35:47 +0000

    javascript

--- basics/variables.js
+++ basics/variables.js
@@ -4,5 +4,12 @@ var studentEmail = "tomarakash@gmail.com"
 studenCity = "Lucknow"
 let studentState;
 
+// studentId=18928 // const never change
+
+
 console.log(studentId)
 console.table([ studentId,studentName,studentEmail,studenCity,studentState])
+/*
+var is not use 
+beacuse of isssue in block scope and functional scope
+*/
