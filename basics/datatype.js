808b9bb80be1fcabc09f0f8be1378acfa0d8f2cc
AKASH TOMAR <121921767+Dfnakash@users.noreply.github.com>
2024-12-11 07:38:46 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+/*
+Each value in a JavaScript program is associated with a data type. A data type specifies the type of data Number, String, Boolean, Double, etc. Each data type requires different amounts of memory and has some specific operations which can be performed over it.
+*/ 
+let name ="Akash"
+let age=21
+let isLoggedIn=false
+
+console.log(typeof age)
+// number=>
+// bigint
+// string=""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol
+// object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
