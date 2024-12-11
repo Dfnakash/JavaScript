2b8cee6c89c770d673f2f983c9b173cb27fb3064
AKASH TOMAR <121921767+Dfnakash@users.noreply.github.com>
2024-12-11 08:53:49 +0000

    operations

--- basics/operationOnConversion.js
+++ basics/operationOnConversion.js
@@ -8,3 +8,20 @@ console.log(valueInNumber);
 // "33"=>33
 //"33abc"=>NaN
 //true =>1; False=>0;
+
+//conversion in Boolean
+let x=1
+console.log(typeof x);
+let isLoggedIn=Boolean(x)
+console.log(typeof isLoggedIn);
+console.log(isLoggedIn);
+
+//conversion in string
+let someNumber=44
+console.log(typeof someNumber)
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber );
+console.log(stringNumber );
+
+//*********************** Opeartions ****************
+
